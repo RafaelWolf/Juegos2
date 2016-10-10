4c9a01c100ae6078a9cdac775ea0dad5271014dd
RafaelWolf <ruzaldivarg@hotmail.com>
2016-10-10 01:48:08 -0500

    collisions
    jh

---
+++ Alice/Assets/Scripts/Empezar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Empezar : MonoBehaviour {
+	public GameObject spawner1;
+	public GameObject spawner2;
+
+	// Use this for initialization
+	void Start () {
+		spawner1.SetActive (false);
+		spawner2.SetActive (false);
+	}
+	
+	public void ActivarSpawners(bool active){
+		if (active) {
+			spawner1.SetActive (true);
+			spawner2.SetActive (true);
+		} else {
+			spawner1.SetActive (false);
+			spawner2.SetActive (false);
+		}
+
+
+	}
+
+
+}
---
+++ Alice/Assets/Scripts/Pintar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pintar : MonoBehaviour {
+	
+
+	void OnCollisionEnter(Collision col) {
+
+
+			Destroy (col.gameObject);
+
+	
+
+
+	}
+}
