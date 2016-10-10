b4ccd0b4542cf435168b80cb0e3723d11d10d632
RafaelWolf <ruzaldivarg@hotmail.com>
2016-10-09 19:24:52 -0500

    Alicia
    Programacion

---
+++ Alice/Assets/Scripts/MoveRoses.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRoses : MonoBehaviour {
+	public float movementSpeed;
+	public bool canMove=true;
+	// Update is called once per frame
+	void Update ()
+	{
+		
+			if (this.canMove) {
+			this.transform.Translate (Vector3.up * this.movementSpeed * Time.deltaTime);
+			}
+
+
+	
+	}
+}
---
+++ Alice/Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject[] enemies;
+	public Vector3 spawnValues;
+	public float spawnWait;
+	public float spawnMostWait;
+	public float spawnLeastWait;
+	public int startWait;
+	public bool stop;
+	public float delay;
+	public GameObject prefab;
+	int randEnemy;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (waitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnWait = Random.Range (spawnLeastWait,spawnMostWait);
+	}
+
+	IEnumerator waitSpawner(){
+		yield return new WaitForSeconds (startWait);
+		while (!stop) {
+			randEnemy = Random.Range (0,2);
+			Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValues.x,spawnValues.x),1,Random.Range(-spawnValues.z,spawnValues.z));
+			prefab=Instantiate(enemies[randEnemy],spawnPosition+transform.TransformPoint(0,0,0),gameObject.transform.rotation) as GameObject;
+			prefab.transform.parent =  GameObject.Find("Plane").transform;
+			Destroy (prefab,delay);
+			yield return new WaitForSeconds (spawnWait);
+		    
+
+
+
+		}
+	}
+
+}
