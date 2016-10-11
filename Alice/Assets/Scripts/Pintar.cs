6751f8f2dc1068c6197dd7cb71868106eb117948
RafaelWolf <ruzaldivarg@hotmail.com>
2016-10-11 17:56:23 -0500

    AliciaCommit
    asdsadsad

--- Alice/Assets/Scripts/Pintar.cs
+++ Alice/Assets/Scripts/Pintar.cs
@@ -3,14 +3,46 @@ using System.Collections;
 
 public class Pintar : MonoBehaviour {
 	
-
+	public  int recarga = 0;
+	public int contadorazul = 0;
+	public int contadorrojas = 0;
 	void OnCollisionEnter(Collision col) {
 
 
-			Destroy (col.gameObject);
+		if (recarga == 0 && ((col.gameObject.tag == "PelotaAzul")||(col.gameObject.tag == "PelotaRoja")))	{
+			Debug.Log ("Recarga amiguito");
+		}
 
 	
 
+		if(col.gameObject.tag=="Cubeta"){
+			Recargar();
+		}
 
+		if ((!recarga == 0)) {
+			if (col.gameObject.tag == "PelotaAzul") {
+				contadorazul++;
+			}
+			if (col.gameObject.tag == "PelotaRoja") {
+				contadorazul++;
+			}
+
+
+	
+
+		}
+			
 	}
+
+	public void Recargar(){
+		recarga = 3;
+	}
+
+
+
+
+
+
+
+
 }
