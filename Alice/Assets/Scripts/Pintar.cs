ecefb95f8387e339864559a5ae40263045d9f830
RafaelWolf <ruzaldivarg@hotmail.com>
2016-10-14 18:15:44 -0500

    Roses y animacion carta

--- Alice/Assets/Scripts/Pintar.cs
+++ Alice/Assets/Scripts/Pintar.cs
@@ -4,33 +4,26 @@ using System.Collections;
 public class Pintar : MonoBehaviour {
 	
 	public  int recarga = 0;
-	public int contadorazul = 0;
 	public int contadorrojas = 0;
 	void OnCollisionEnter(Collision col) {
 
-
-		if (recarga == 0 && ((col.gameObject.tag == "PelotaAzul")||(col.gameObject.tag == "PelotaRoja")))	{
+		if (recarga == 0 && ((col.gameObject.tag == "Rosa1R")||(col.gameObject.tag == "Rosa2R")||(col.gameObject.tag == "Rosa3R")||(col.gameObject.tag == "Rosa4R")||(col.gameObject.tag == "Rosa5R")||(col.gameObject.tag == "Rosa1B")||(col.gameObject.tag == "Rosa2B")||(col.gameObject.tag == "Rosa3B")||(col.gameObject.tag == "Rosa4B")||(col.gameObject.tag == "Rosa5B")))	{
 			Debug.Log ("Recarga amiguito");
 		}
-
-	
-
+		if ((recarga != 0)) {
+			if ((col.gameObject.tag == "Rosa1R")||(col.gameObject.tag == "Rosa2R")||(col.gameObject.tag == "Rosa3R")||(col.gameObject.tag == "Rosa4R")||(col.gameObject.tag == "Rosa5R")) {
+				contadorrojas++;
+				recarga--;
+			}
+			if ((col.gameObject.tag == "Rosa1B")||(col.gameObject.tag == "Rosa2B")||(col.gameObject.tag == "Rosa3B")||(col.gameObject.tag == "Rosa4B")||(col.gameObject.tag == "Rosa5B")) {
+				contadorrojas--;
+				recarga--;
+			}
+		}
 		if(col.gameObject.tag=="Cubeta"){
 			Recargar();
 		}
-
-		if ((recarga != 0)) {
-			if (col.gameObject.tag == "PelotaAzul") {
-				contadorazul++;
-			}
-			if (col.gameObject.tag == "PelotaRoja") {
-				contadorazul++;
-			}
-
-
-	
-
-		}
+		Debug.Log ("Marcador"+contadorrojas.ToString());
 			
 	}
 
